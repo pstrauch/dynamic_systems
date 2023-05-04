22d170d63b7bfbcdf5162ee251eac1d831c4ed98
Pascal <pstrauch@ethz.ch>
2023-05-04 11:17:33 +0200

    Cast to void to avoid warnings. Change formatting

--- src/test.cpp
+++ src/test.cpp
@@ -5,6 +5,9 @@
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+  (void) argc;
+  (void) argv;
+  
+  std::cout << "Hello World!" << std::endl;
+  return 0;
 }
